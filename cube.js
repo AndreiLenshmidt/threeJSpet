48d189aa35441e48e22f8c67408635a2eec5f6ce
Lenshmidt Andrei <lenshmidt.andrei@gmail.com>
2025-03-04 17:09:47 +0300

    3d модель

--- cube.js
+++ cube.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
 // Создаем оси координат и добавляем их на экран (сцену)
@@ -16,49 +17,53 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setAnimationLoop(animate);
 document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00,
-  wireframe: true,
-});
-const cube = new THREE.Mesh(geometry, material);
-// изменяем позицию объекта
-cube.position.x = -1;
-cube.position.y = 2;
-cube.position.z = 1;
-// длина вектора
-console.log("длина вектора", cube.position.length());
-// расстояние до другого вектора
-console.log(
-  "расстояние до другого вектора",
-  cube.position.distanceTo(camera.position)
-);
-// нормализованное значение
-console.log("нормализованное значение", cube.position.normalize());
-// быстрое изменение координат
-console.log("быстрое изменение координат", cube.position.set(0, 0, 0));
-
-// изменяем масшаб объкта по осям:
-cube.scale.x = 0.5;
-cube.scale.y = 1.2;
-cube.scale.z = 0.6;
-// изменяем масшаб объкта по всем осям:
-cube.scale.set(1, 1, 1);
-
-scene.add(cube);
-// Позиция камеры ближе/дальше от экрана, при 0 камера внутри объекта
-camera.position.z = 5;
+camera.position.set(0, 2, 5);
 // Позиция обзора камеры lookAt передаем вектор или позицию
 // camera.lookAt(new THREE.Vector3(2, 0, 0));
 // camera.lookAt(cube.position);
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(10, 10),
+  new THREE.MeshStandardMaterial({
+    color: "#444",
+    metalness: 0,
+    roughness: 0.5,
+  })
+);
+
+floor.receiveShadow = true;
+floor.rotation.x = -Math.PI * 0.5;
+scene.add(floor);
+
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.61);
+hemiLight.position.set(0, 50, 0);
+scene.add(hemiLight);
+
+const dirLight = new THREE.DirectionalLight(0xffffff, 0.54);
+dirLight.position.set(-8, 12, 8);
+dirLight.castShadow = true;
+dirLight.shadow.mapSize = new THREE.Vector2(1024, 1024);
+scene.add(dirLight);
+
+const loader = new GLTFLoader();
+loader.load(
+  "/FinalBaseMesh.glb",
+  function (gltf) {
+    // scene.add(gltf.scene);
+    console.log("suscess");
+    console.log(gltf);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 function animate() {
   //  Изменить порядок вращения по умолчанию позволяет свойство reorder
   //   cube.rotation.reorder("XYZ");
   // вращаем объект при помощи свойства rotation. Порядок вращения по умолчанию : XYZ
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
 
   renderer.render(scene, camera);
 }
